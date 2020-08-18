237105a58a0fe4ab9f73cb31a7bc62537fb1b5cb
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-17 21:53:33 -0500

    finally set up with eslint

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+// import react and react dom
+import React from 'react';
+import ReactDom from 'react-dom';
+
+//creat root const
+const root = document.getElementById('app');
+
+ReactDom.render(<h1>Finally HEllo!!!</h1>, root);
