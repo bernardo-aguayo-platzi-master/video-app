6a0066f86a6cbaa43281bc5ef917db3d8e8da32c
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-18 16:22:50 -0500

    create carousel component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,13 @@
+// import react 
+import React from 'react';
+
+// functional component
+export default function Carousel({ children }) {
+  return (
+    <section className='carousel'>
+      <div className='carousel__container'>
+        {children}
+      </div>
+    </section>
+  );
+}
