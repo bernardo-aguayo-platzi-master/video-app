172320cdeffd04c53fb6f291ee4c8504c12b5818
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-20 16:48:38 -0500

    add icons

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,6 +1,9 @@
 // import react
 import React from 'react';
 
+import plusIcon from '../assets/plus-icon.png'
+import playIcon from '../assets/play-icon.png'
+
 //functional component
 export default function CarouselItem({ image, id }) {
   return (
@@ -8,8 +11,8 @@ export default function CarouselItem({ image, id }) {
       <img className='carousel-item__img' src={image} alt='' />
       <div className='carousel-item__details'>
         <div>
-          <img className='carousel-item__details--img' src='' alt='' />
-          <img className='carousel-item__details--img' src='' alt='' />
+          <img className='carousel-item__details--img' src={playIcon} alt='' />
+          <img className='carousel-item__details--img' src={plusIcon} alt='' />
         </div>
         <p className='carousel-item__details--title'>Title</p>
         <p className='carousel-item__details--subtitle'>2019 16+ 114 minutes  </p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,16 @@
 // import react
 import React from 'react';
 
+import user from '../assets/user-icon.png'
+
 //functional component
 export default function Header() {
   return (
     <header className='header'>
-      <img className='header__img' src='' alt='' />
+      <img className='header__img' src='/' alt='' />
       <div className='header__menu'>
         <div className='header__menu--profile'>
-          <img src='' alt='' />
+          <img src={user} />
           <p>Profile</p>
         </div>
         <ul>
