6c5993323f4b2d5f25b8b281c68e0b4379b6033c
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-21 12:09:17 -0500

    prepare react redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
-/* eslint-disable no-unused-vars */
 // import react and react dom
 import React from 'react';
 import ReactDom from 'react-dom';
+
+//import provider
+import { Provider } from 'react-redux';
+
+//import store
+import { createStore } from 'redux'
 
 // import app
 import App from './routes/App'
@@ -12,4 +17,8 @@ import './index.css'
 // creat root const
 const root = document.getElementById('app');
 
-ReactDom.render(<App />, root);
+ReactDom.render(
+  <Provider>
+    <App />
+  </Provider>
+  , root);
